8515a02a5927521229cc76b101f355d547011db6
Kirtikumar A <avaiyakirtib@gmail.com>
2016-11-03 16:45:46 +0530

    Add espresso test files

---
+++ app/src/androidTest/java/com/mahii/circletest/ChangeTextBehaviorTest.java
@@ -0,0 +1,59 @@
+package com.mahii.circletest;
+
+import android.support.test.rule.ActivityTestRule;
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class ChangeTextBehaviorTest {
+
+    private static final String STRING_TO_BE_TYPED = "Espresso";
+
+    /**
+     * A JUnit {@link Rule @Rule} to launch your activity under test. This is a replacement
+     * for {@link ActivityInstrumentationTestCase2}.
+     * <p>
+     * Rules are interceptors which are executed for each test method and will run before
+     * any of your setup code in the {@link Before @Before} method.
+     * <p>
+     * {@link ActivityTestRule} will create and launch of the activity for you and also expose
+     * the activity under test. To get a reference to the activity you can use
+     * the {@link ActivityTestRule#getActivity()} method.
+     */
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void changeText_sameActivity() {
+
+        // Type text and then press the button.
+        onView(withId(R.id.editTextUserInput)).perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
+        onView(withId(R.id.changeTextBt)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)));
+
+    }
+
+    @Test
+    public void changeText_newActivity() {
+
+        // Type text and then press the button.
+        onView(withId(R.id.editTextUserInput)).perform(typeText(STRING_TO_BE_TYPED), closeSoftKeyboard());
+        onView(withId(R.id.activityChangeTextBtn)).perform(click());
+
+        // This view is in a different Activity, no need to tell Espresso.
+        onView(withId(R.id.show_text_view)).check(matches(withText(STRING_TO_BE_TYPED)));
+
+    }
+}
